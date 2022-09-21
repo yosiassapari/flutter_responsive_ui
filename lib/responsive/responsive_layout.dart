9bde61fbf39943a2ca1e5b5d6dc0bb232f2f7466
yosiassapari <yosiassapari@gmail.com>
2022-09-21 14:54:57 +0700

    responsive setting

---
+++ lib/responsive/responsive_layout.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_app/responsive/dimensions.dart';
+
+class ResponsiveLayout extends StatelessWidget {
+  const ResponsiveLayout({
+    Key? key,
+    required this.mobileBody,
+    required this.desktopBody,
+  }) : super(key: key);
+
+  final Widget mobileBody;
+  final Widget desktopBody;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        if (constraints.maxWidth < mobileWidth) {
+          return mobileBody;
+        } else {
+          return desktopBody;
+        }
+      },
+    );
+  }
+}
